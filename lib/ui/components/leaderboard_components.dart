8fa1b8d7a182683edba2c8cf1a8adb3fc6c7402a
Dzakwan Irfan <lexdani368@gmail.com>
2024-11-19 08:44:27 +0700

    refactor: optimize leaderboard UI components and structure
    - Split components & pages for better modularity
    - Convert to StatelessWidget where possible
    - Extract reusable components
    - Improve code organization & performance

--- lib/ui/components/leaderboard_components.dart
+++ lib/ui/components/leaderboard_components.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_launcher_icons/xml_templates.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:temulik/constants/colors.dart';
 
-class TabLeaderBoard extends StatefulWidget {
+class TabLeaderBoard extends StatelessWidget {
   final String tab1;
   final String tab2;
   final Widget page1;
@@ -18,11 +17,6 @@ class TabLeaderBoard extends StatefulWidget {
   });
 
   @override
-  State<TabLeaderBoard> createState() => _TabLeaderBoardState();
-}
-
-class _TabLeaderBoardState extends State<TabLeaderBoard> {
-  @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
@@ -34,28 +28,29 @@ class _TabLeaderBoardState extends State<TabLeaderBoard> {
             labelColor: AppColors.green,
             unselectedLabelColor: Colors.black,
             indicatorSize: TabBarIndicatorSize.tab,
-            overlayColor: WidgetStatePropertyAll(Colors.transparent),
+            overlayColor: const WidgetStatePropertyAll(Colors.transparent),
             dividerColor: Colors.transparent,
             tabs: [
-              Tab(text: widget.tab1),
-              Tab(text: widget.tab2),
+              Tab(text: tab1),
+              Tab(text: tab2),
             ],
           ),
           title: const Center(child: Text('Leaderboard')),
           elevation: 0,
         ),
         body: TabBarView(
-          children: [
-            widget.page1,
-            widget.page2,
-          ],
+          children: [page1, page2],
         ),
       ),
     );
   }
 }
 
-class LeaderboardCard extends StatefulWidget {
+class LeaderboardCard extends StatelessWidget {
+  static const double _borderRadius = 6.0;
+  static const double _padding = 12.0;
+  static const double _avatarRadius = 20.0;
+
   final int rank;
   final String name;
   final String faculty;
@@ -74,255 +69,277 @@ class LeaderboardCard extends StatefulWidget {
   });
 
   @override
-  State<LeaderboardCard> createState() => _LeaderboardCardState();
-}
-
-class _LeaderboardCardState extends State<LeaderboardCard> {
-  @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: widget.onTap,
+      onTap: onTap,
       child: Stack(
         children: [
-          Container(
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.black,
-                width: 1.0,
-              ),
-              borderRadius: BorderRadius.circular(6.0),
-              gradient: LinearGradient(
-                colors: [
-                  Color(0xFFDDE6F7),
-                  Color(0xFFC9D6E9),
-                ],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-              ),
-            ),
-            padding: EdgeInsets.symmetric(
-              horizontal: 12.0,
-              vertical: 6.0,
-            ),
-            margin: EdgeInsets.only(
-              top: 6.0,
-              left: 6.0,
-              right: 6.0,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Rank(rank: widget.rank),
-                    SizedBox(width: 12.00),
-                    Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(
-                          color: AppColors.coral,
-                          width: 2.0,
-                        ),
-                      ),
-                      child: CircleAvatar(
-                        radius: 20.0,
-                        backgroundImage: AssetImage(widget.image),
-                        backgroundColor: Colors.white,
-                      ),
-                    ),
-                    SizedBox(width: 12.00),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Stack(
-                          alignment: Alignment.center,
-                          children: [
-                            Text(
-                              widget.name,
-                              style: TextStyle(
-                                fontSize: 16.00,
-                                foreground: Paint()
-                                  ..style = PaintingStyle.stroke
-                                  ..strokeWidth = 3.0
-                                  ..color = Colors.black,
-                              ),
-                            ),
-                            Text(
-                              widget.name,
-                              style: TextStyle(
-                                fontSize: 16.00,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Text(
-                          widget.faculty,
-                          style: TextStyle(
-                            fontSize: 12.00,
-                            color: AppColors.coral,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 16.0,
-                    vertical: 3.0,
-                  ),
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        Color(0xFF8D9EB8),
-                        Color(0xFFA1B1C8),
-                      ],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                    ),
-                    borderRadius: BorderRadius.circular(4.0),
-                    boxShadow: [
-                      const BoxShadow(
-                        color: Colors.black,
-                      ),
-                      const BoxShadow(
-                        color: Colors.black,
-                        spreadRadius: -16.0,
-                        blurRadius: 20.0,
-                      ),
-                    ],
-                  ),
-                  child: Center(
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        Text(
-                          widget.points.toString(),
-                          style: TextStyle(
-                            fontSize: 20.00,
-                            foreground: Paint()
-                              ..style = PaintingStyle.stroke
-                              ..strokeWidth = 3.0
-                              ..color = Colors.black,
-                          ),
-                        ),
-                        Text(
-                          widget.points.toString(),
-                          style: TextStyle(
-                            fontSize: 20.00,
-                            fontWeight: FontWeight.w800,
-                            color: AppColors.lavender,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            bottom: 0.5,
-            right: 115.0,
-            child: SvgPicture.asset(
-              'soedirman.svg',
-              height: 50.0,
-              colorFilter: ColorFilter.mode(
-                AppColors.sky,
-                BlendMode.srcIn,
-              ),
-            ),
-          ),
+          _buildCardContent(),
+          _buildWatermark(),
         ],
       ),
     );
   }
+
+  Widget _buildCardContent() {
+    return Container(
+      decoration: _buildCardDecoration(),
+      padding: const EdgeInsets.symmetric(
+        horizontal: _padding,
+        vertical: 6.0,
+      ),
+      margin: const EdgeInsets.only(
+        top: 6.0,
+        left: 6.0,
+        right: 6.0,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          _buildUserInfo(),
+          _buildPointsDisplay(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildUserInfo() {
+    return Row(
+      children: [
+        RankDisplay(rank: rank),
+        const SizedBox(width: _padding),
+        _buildAvatar(),
+        const SizedBox(width: _padding),
+        _buildNameAndFaculty(),
+      ],
+    );
+  }
+
+  Widget _buildAvatar() {
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: AppColors.coral,
+          width: 2.0,
+        ),
+      ),
+      child: CircleAvatar(
+        radius: _avatarRadius,
+        backgroundImage: AssetImage(image),
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+
+  Widget _buildNameAndFaculty() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        StrokedText(text: name, fontSize: 16.0),
+        Text(
+          faculty,
+          style: const TextStyle(
+            fontSize: 12.00,
+            color: AppColors.coral,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildPointsDisplay() {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 16.0,
+        vertical: 3.0,
+      ),
+      decoration: _buildPointsDecoration(),
+      child: Center(
+        child: StrokedText(
+          text: points.toString(),
+          fontSize: 20.0,
+          textColor: AppColors.lavender,
+          fontWeight: FontWeight.w800,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildWatermark() {
+    return Positioned(
+      bottom: 0.5,
+      right: 115.0,
+      child: SvgPicture.asset(
+        'soedirman.svg',
+        height: 50.0,
+        colorFilter: ColorFilter.mode(
+          AppColors.sky,
+          BlendMode.srcIn,
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _buildCardDecoration() {
+    return BoxDecoration(
+      border: Border.all(
+        color: Colors.black,
+        width: 1.0,
+      ),
+      borderRadius: BorderRadius.circular(_borderRadius),
+      gradient: const LinearGradient(
+        colors: [
+          Color(0xFFDDE6F7),
+          Color(0xFFC9D6E9),
+        ],
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+      ),
+    );
+  }
+
+  BoxDecoration _buildPointsDecoration() {
+    return BoxDecoration(
+      gradient: const LinearGradient(
+        colors: [
+          Color(0xFF8D9EB8),
+          Color(0xFFA1B1C8),
+        ],
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+      ),
+      borderRadius: BorderRadius.circular(4.0),
+      boxShadow: const [
+        BoxShadow(
+          color: Colors.black,
+        ),
+        BoxShadow(
+          color: Colors.black,
+          spreadRadius: -16.0,
+          blurRadius: 20.0,
+        ),
+      ],
+    );
+  }
 }
 
-class Rank extends StatelessWidget {
-  final rank;
-  const Rank({
+class RankDisplay extends StatelessWidget {
+  final int rank;
+
+  const RankDisplay({
     super.key,
     required this.rank,
   });
 
-  List<Color> rankColor() {
+  List<Color> _getRankColors() {
     switch (rank) {
       case 1:
-        return [AppColors.gold, Color(0xFFF8DE79), Color(0xFFEEBF33)];
+        return [
+          AppColors.gold,
+          const Color(0xFFF8DE79),
+          const Color(0xFFEEBF33)
+        ];
       case 2:
-        return [AppColors.silver, Color(0xFFE2EAF7), Color(0xFFCFDBEC)];
+        return [
+          AppColors.silver,
+          const Color(0xFFE2EAF7),
+          const Color(0xFFCFDBEC)
+        ];
       case 3:
-        return [AppColors.bronze, Color(0xFFEDA367), Color(0xFFDF9355)];
+        return [
+          AppColors.bronze,
+          const Color(0xFFEDA367),
+          const Color(0xFFDF9355)
+        ];
       default:
-        return [AppColors.darkGrey, Color(0xFFA1B1C8), Color(0xFF8D9EB8)];
+        return [
+          AppColors.darkGrey,
+          const Color(0xFFA1B1C8),
+          const Color(0xFF8D9EB8)
+        ];
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    final colors = _getRankColors();
     return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(4.0),
+        border: Border.all(
+          width: 2.0,
+          color: AppColors.sky,
+        ),
+      ),
       child: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 12.0,
+          vertical: 3.0,
+        ),
         decoration: BoxDecoration(
+          color: colors[0],
           borderRadius: BorderRadius.circular(4.0),
           border: Border.all(
             width: 2.0,
-            color: AppColors.sky,
-            style: BorderStyle.solid,
+            color: colors[2],
+          ),
+          gradient: LinearGradient(
+            colors: [colors[1], colors[2]],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
           ),
         ),
-        child: Container(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 12.0,
-            vertical: 3.0,
-          ),
-          decoration: BoxDecoration(
-            color: rankColor()[0],
-            borderRadius: BorderRadius.circular(4.0),
-            border: Border.all(
-              width: 2.0,
-              color: rankColor()[2],
-              style: BorderStyle.solid,
-            ),
-            gradient: LinearGradient(
-              colors: [
-                rankColor()[1],
-                rankColor()[2],
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            ),
-          ),
-          child: Center(
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Text(
-                  rank.toString(),
-                  style: TextStyle(
-                    fontSize: 20.00,
-                    foreground: Paint()
-                      ..style = PaintingStyle.stroke
-                      ..strokeWidth = 3.0
-                      ..color = Colors.black,
-                  ),
-                ),
-                Text(
-                  rank.toString(),
-                  style: TextStyle(
-                    fontSize: 20.00,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
+        child: Center(
+          child: StrokedText(
+            text: rank.toString(),
+            fontSize: 20.0,
           ),
         ),
       ),
+    );
+  }
+}
+
+class StrokedText extends StatelessWidget {
+  final String text;
+  final double fontSize;
+  final Color textColor;
+  final FontWeight fontWeight;
+
+  const StrokedText({
+    super.key,
+    required this.text,
+    required this.fontSize,
+    this.textColor = Colors.white,
+    this.fontWeight = FontWeight.bold,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Text(
+          text,
+          style: TextStyle(
+            fontSize: fontSize,
+            foreground: Paint()
+              ..style = PaintingStyle.stroke
+              ..strokeWidth = 3.0
+              ..color = Colors.black,
+          ),
+        ),
+        Text(
+          text,
+          style: TextStyle(
+            fontSize: fontSize,
+            fontWeight: fontWeight,
+            color: textColor,
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/ui/leaderboard_page.dart
+++ lib/ui/leaderboard_page.dart
@@ -1,52 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:temulik/constants/colors.dart';
 import 'package:temulik/ui/components/datas.dart';
 import 'package:temulik/ui/components/leaderboard_components.dart';
 
-class LeaderboardPage extends StatefulWidget {
+class LeaderboardPage extends StatelessWidget {
   const LeaderboardPage({super.key});
 
   @override
-  State<LeaderboardPage> createState() => _LeaderboardPageState();
-}
-
-class _LeaderboardPageState extends State<LeaderboardPage> {
-  @override
   Widget build(BuildContext context) {
-    return TabLeaderBoard(
+    return const TabLeaderBoard(
       tab1: 'Bulan ini',
       tab2: 'Keseluruhan waktu',
-      page1: BulanIni(),
-      page2: KeseluruhanWaktu(),
+      page1: MonthlyLeaderboard(),
+      page2: AllTimeLeaderboard(),
     );
   }
 }
 
-// Tab Bulan ini
-class BulanIni extends StatefulWidget {
-  BulanIni({super.key});
+class MonthlyLeaderboard extends StatelessWidget {
+  const MonthlyLeaderboard({super.key});
 
   @override
-  State<BulanIni> createState() => _BulanIniState();
-}
-
-class _BulanIniState extends State<BulanIni> {
-  void _showLeaderboardBottomSheet(
-      BuildContext context, Map<String, dynamic> userData) {
-    showModalBottomSheet(
-      context: context,
-      isScrollControlled: true,
-      backgroundColor: Colors.white,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(40)),
-      ),
-      constraints: BoxConstraints(
-        maxHeight: MediaQuery.of(context).size.height * 0.5,
-      ),
-      builder: (context) => LeaderboardDetail(userData: userData),
+  Widget build(BuildContext context) {
+    return LeaderboardContent(
+      leaderboardData: bulanIniList,
     );
   }
+}
+
+class AllTimeLeaderboard extends StatelessWidget {
+  const AllTimeLeaderboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LeaderboardContent(
+      leaderboardData: keseluruhanWaktuList,
+    );
+  }
+}
+
+class LeaderboardContent extends StatelessWidget {
+  final List<Map<String, dynamic>> leaderboardData;
+
+  const LeaderboardContent({
+    super.key,
+    required this.leaderboardData,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -60,21 +59,19 @@ class _BulanIniState extends State<BulanIni> {
         ),
         Expanded(
           child: Container(
-            // padding: const EdgeInsets.only(bottom: 6.0),
             color: AppColors.sky,
             child: ListView.builder(
               physics: const AlwaysScrollableScrollPhysics(),
-              itemCount: bulanIniList.length,
+              itemCount: leaderboardData.length,
               itemBuilder: (context, index) {
-                final leaderboard = bulanIniList[index];
+                final data = leaderboardData[index];
                 return LeaderboardCard(
-                  rank: leaderboard['rank'],
-                  name: leaderboard['name'],
-                  faculty: leaderboard['faculty'],
-                  points: leaderboard['points'],
-                  image: leaderboard['image'],
-                  onTap: () =>
-                      _showLeaderboardBottomSheet(context, leaderboard),
+                  rank: data['rank'],
+                  name: data['name'],
+                  faculty: data['faculty'],
+                  points: data['points'],
+                  image: data['image'],
+                  onTap: () => _showLeaderboardDetail(context, data),
                 );
               },
             ),
@@ -83,18 +80,8 @@ class _BulanIniState extends State<BulanIni> {
       ],
     );
   }
-}
 
-// Tab Keseluruhan waktu
-class KeseluruhanWaktu extends StatefulWidget {
-  const KeseluruhanWaktu({super.key});
-
-  @override
-  State<KeseluruhanWaktu> createState() => _KeseluruhanWaktuState();
-}
-
-class _KeseluruhanWaktuState extends State<KeseluruhanWaktu> {
-  void _showLeaderboardBottomSheet(
+  void _showLeaderboardDetail(
       BuildContext context, Map<String, dynamic> userData) {
     showModalBottomSheet(
       context: context,
@@ -107,42 +94,6 @@ class _KeseluruhanWaktuState extends State<KeseluruhanWaktu> {
         maxHeight: MediaQuery.of(context).size.height * 0.5,
       ),
       builder: (context) => LeaderboardDetail(userData: userData),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Image.asset(
-          'pahlawan_gensoed.png',
-          width: double.infinity,
-          fit: BoxFit.cover,
-          filterQuality: FilterQuality.high,
-        ),
-        Expanded(
-          child: Container(
-            // padding: const EdgeInsets.only(bottom: 6.0),
-            color: AppColors.sky,
-            child: ListView.builder(
-              physics: const AlwaysScrollableScrollPhysics(),
-              itemCount: keseluruhanWaktuList.length,
-              itemBuilder: (context, index) {
-                final leaderboard = keseluruhanWaktuList[index];
-                return LeaderboardCard(
-                  rank: leaderboard['rank'],
-                  name: leaderboard['name'],
-                  faculty: leaderboard['faculty'],
-                  points: leaderboard['points'],
-                  image: leaderboard['image'],
-                  onTap: () =>
-                      _showLeaderboardBottomSheet(context, leaderboard),
-                );
-              },
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
@@ -151,9 +102,9 @@ class LeaderboardDetail extends StatelessWidget {
   final Map<String, dynamic> userData;
 
   const LeaderboardDetail({
-    Key? key,
+    super.key,
     required this.userData,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -162,92 +113,92 @@ class LeaderboardDetail extends StatelessWidget {
       width: double.infinity,
       child: Column(
         children: [
-          // Handle bar indicator
-          Container(
-            width: 40,
-            height: 4,
-            margin: const EdgeInsets.only(bottom: 20),
-            decoration: BoxDecoration(
-              color: Colors.grey[300],
-              borderRadius: BorderRadius.circular(2),
-            ),
-          ),
+          _buildHandleBar(),
           const SizedBox(height: 12.0),
-          Column(
-            children: [
-              Container(
-                width: 120.0,
-                height: 120.0,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                    color: AppColors.green,
-                    width: 4.0,
-                  ),
-                ),
-                child: CircleAvatar(
-                  radius: 40.0,
-                  backgroundImage: AssetImage(userData['image']),
-                  backgroundColor: Colors.white,
-                ),
-              ),
-              const SizedBox(height: 12.0),
-              Column(
-                children: [
-                  Text(
-                    userData['name'],
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: AppColors.dark,
-                    ),
-                  ),
-                  const SizedBox(height: 12.0),
-                  Column(
-                    children: [
-                      Text(
-                        userData['nim'],
-                        style: TextStyle(
-                          color: AppColors.dark,
-                          fontSize: 16,
-                        ),
-                      ),
-                      const SizedBox(height: 3.0),
-                      Text(
-                        userData['faculty'],
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: AppColors.dark,
-                        ),
-                      ),
-                      const SizedBox(height: 3.0),
-                      Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            userData['no'],
-                            style: TextStyle(
-                              color: AppColors.dark,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const SizedBox(width: 4),
-                          Icon(
-                            Icons.open_in_new,
-                            size: 20,
-                            color: AppColors.dark,
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          ),
+          _buildUserProfile(),
         ],
       ),
+    );
+  }
+
+  Widget _buildHandleBar() {
+    return Container(
+      width: 40,
+      height: 4,
+      margin: const EdgeInsets.only(bottom: 20),
+      decoration: BoxDecoration(
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.circular(2),
+      ),
+    );
+  }
+
+  Widget _buildUserProfile() {
+    return Column(
+      children: [
+        _buildProfileImage(),
+        const SizedBox(height: 12.0),
+        _buildUserInfo(),
+      ],
+    );
+  }
+
+  Widget _buildProfileImage() {
+    return Container(
+      width: 120.0,
+      height: 120.0,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: AppColors.green,
+          width: 4.0,
+        ),
+      ),
+      child: CircleAvatar(
+        radius: 40.0,
+        backgroundImage: AssetImage(userData['image']),
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+
+  Widget _buildUserInfo() {
+    const textStyle = TextStyle(
+      color: AppColors.dark,
+      fontSize: 16,
+    );
+
+    return Column(
+      children: [
+        Text(
+          userData['name'],
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: AppColors.dark,
+          ),
+        ),
+        const SizedBox(height: 12.0),
+        Text(userData['nim'], style: textStyle),
+        const SizedBox(height: 3.0),
+        Text(userData['faculty'], style: textStyle),
+        const SizedBox(height: 3.0),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              userData['no'],
+              style: textStyle.copyWith(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(width: 4),
+            const Icon(
+              Icons.open_in_new,
+              size: 20,
+              color: AppColors.dark,
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
