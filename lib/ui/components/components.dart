1470982a5f10a00bbadbea2abe3684a192236386
Dzakwan Irfan <lexdani368@gmail.com>
2024-11-28 13:12:40 +0700

    feat: menyelesaikan front-end penemuan_form_page.dart

--- lib/ui/components/components.dart
+++ lib/ui/components/components.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:temulik/constants/colors.dart';
+import 'package:intl/intl.dart';
 
 class DoneButton extends StatelessWidget {
   final VoidCallback onPressed;
@@ -120,8 +121,30 @@ class TextTinyMedium extends StatelessWidget {
       text,
       style: TextStyle(
         fontSize: 12.0,
-        fontWeight: FontWeight.bold,
+        fontWeight: FontWeight.w500,
         color: AppColors.dark,
+      ),
+    );
+  }
+}
+
+class TextSmallMedium extends StatelessWidget {
+  final String text;
+  final Color color;
+  const TextSmallMedium({
+    super.key,
+    required this.text,
+    this.color = AppColors.darkest,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: 16.0,
+        fontWeight: FontWeight.w500,
+        color: color,
       ),
     );
   }
@@ -276,6 +299,461 @@ class _CustomImageSliderState extends State<CustomImageSlider> {
           ),
         ],
       ),
+    );
+  }
+}
+
+class LaporAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  const LaporAppBar({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leadingWidth: 70.0,
+      leading: IconButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        icon: Icon(
+          Icons.close,
+          color: AppColors.darkest,
+          size: 20.0,
+        ),
+      ),
+      title: Center(
+        child: TextBold(
+          text: title,
+          color: AppColors.darkest,
+        ),
+      ),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.only(right: 20.0),
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.blue,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(64.0),
+              ),
+            ),
+            child: TextSmallMedium(
+              text: 'Kirim',
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(80);
+}
+
+class InputForm extends StatelessWidget {
+  final String label;
+  final String hintText;
+  const InputForm({super.key, required this.label, required this.hintText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextSmallMedium(text: label),
+          TextFormField(
+            decoration: InputDecoration(
+              hintText: hintText,
+              hintStyle: TextStyle(
+                color: AppColors.darkGrey,
+                fontSize: 16.0,
+              ),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: BorderSide(
+                    color: AppColors.grey,
+                  )),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(
+                  color: AppColors.green,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SelectForm extends StatelessWidget {
+  final String label;
+  final String hintText;
+  final List<String> items;
+  final void Function(String?) onChanged;
+  final String? value;
+
+  const SelectForm({
+    super.key,
+    required this.label,
+    required this.hintText,
+    required this.items,
+    required this.onChanged,
+    this.value,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TextSmallMedium(text: label),
+        ButtonTheme(
+          alignedDropdown: true,
+          child: DropdownButtonFormField<String>(
+            value: value,
+            icon: const Icon(Icons.keyboard_arrow_down),
+            hint: Text(
+              hintText,
+              style: const TextStyle(
+                color: AppColors.darkGrey,
+                fontSize: 16.0,
+              ),
+            ),
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: const BorderSide(color: AppColors.grey),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: const BorderSide(color: AppColors.green),
+              ),
+            ),
+            items: items
+                .map((String item) => DropdownMenuItem<String>(
+                      value: item,
+                      child: Text(
+                        item,
+                        style: const TextStyle(
+                          fontSize: 16.0,
+                          color: AppColors.darkest,
+                        ),
+                      ),
+                    ))
+                .toList(),
+            onChanged: onChanged,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TextAreaForm extends StatefulWidget {
+  final String label;
+  final String hintText;
+  final int maxLength;
+  final void Function(String)? onChanged;
+
+  const TextAreaForm({
+    super.key,
+    required this.label,
+    required this.hintText,
+    this.maxLength = 100,
+    this.onChanged,
+  });
+
+  @override
+  State<TextAreaForm> createState() => _TextAreaFormState();
+}
+
+class _TextAreaFormState extends State<TextAreaForm> {
+  final TextEditingController _controller = TextEditingController();
+  int _textLength = 0;
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TextSmallMedium(text: widget.label),
+        Stack(
+          children: [
+            TextFormField(
+              controller: _controller,
+              maxLength: widget.maxLength,
+              maxLines: 3,
+              onChanged: (value) {
+                setState(() {
+                  _textLength = value.length;
+                });
+                if (widget.onChanged != null) {
+                  widget.onChanged!(value);
+                }
+              },
+              decoration: InputDecoration(
+                hintText: widget.hintText,
+                hintStyle: const TextStyle(
+                  color: AppColors.darkGrey,
+                  fontSize: 16.0,
+                ),
+                counterText: '', // Sembunyikan counter default
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(
+                    color: AppColors.grey,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(
+                    color: AppColors.green,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              right: 8,
+              bottom: 8,
+              child: Text(
+                '$_textLength/${widget.maxLength}',
+                style: const TextStyle(
+                  color: AppColors.darkGrey,
+                  fontSize: 14.0,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class DatePickerForm extends StatelessWidget {
+  final String label;
+  final String hintText;
+  final DateTime? selectedDate;
+  final void Function(DateTime?)? onChanged;
+
+  const DatePickerForm({
+    super.key,
+    required this.label,
+    required this.hintText,
+    this.selectedDate,
+    this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TextSmallMedium(text: label),
+        InkWell(
+          onTap: () async {
+            final DateTime? picked = await showDatePicker(
+              context: context,
+              initialDate: selectedDate ?? DateTime.now(),
+              firstDate: DateTime(2000),
+              lastDate: DateTime.now(),
+            );
+            if (picked != null && onChanged != null) {
+              onChanged!(picked);
+            }
+          },
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+            decoration: BoxDecoration(
+              border: Border.all(color: AppColors.grey),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  selectedDate != null
+                      ? DateFormat('dd/MM/yyyy').format(selectedDate!)
+                      : hintText,
+                  style: TextStyle(
+                    color: selectedDate != null
+                        ? AppColors.darkest
+                        : AppColors.darkGrey,
+                    fontSize: 16.0,
+                  ),
+                ),
+                const Icon(Icons.calendar_today, color: AppColors.darkGrey),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TimePickerForm extends StatelessWidget {
+  final String label;
+  final String hintText;
+  final TimeOfDay? selectedTime;
+  final void Function(TimeOfDay?)? onChanged;
+
+  const TimePickerForm({
+    super.key,
+    required this.label,
+    required this.hintText,
+    this.selectedTime,
+    this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TextSmallMedium(text: label),
+        InkWell(
+          onTap: () async {
+            final TimeOfDay? picked = await showTimePicker(
+              context: context,
+              initialTime: selectedTime ?? TimeOfDay.now(),
+            );
+            if (picked != null && onChanged != null) {
+              onChanged!(picked);
+            }
+          },
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+            decoration: BoxDecoration(
+              border: Border.all(color: AppColors.grey),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  selectedTime != null
+                      ? selectedTime!.format(context)
+                      : hintText,
+                  style: TextStyle(
+                    color: selectedTime != null
+                        ? AppColors.darkest
+                        : AppColors.darkGrey,
+                    fontSize: 16.0,
+                  ),
+                ),
+                const Icon(Icons.access_time, color: AppColors.darkGrey),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ImagePickerForm extends StatelessWidget {
+  final String label;
+  final String hintText;
+  final String? imagePath;
+  final Function(String?)? onImageSelected;
+
+  const ImagePickerForm({
+    super.key,
+    required this.label,
+    required this.hintText,
+    this.imagePath,
+    this.onImageSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TextSmallMedium(text: label),
+        InkWell(
+          onTap: () {
+            // Implementasi pemilihan foto akan ditambahkan
+          },
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+            decoration: BoxDecoration(
+              border: Border.all(color: AppColors.grey),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  imagePath ?? hintText,
+                  style: TextStyle(
+                    color: imagePath != null
+                        ? AppColors.darkest
+                        : AppColors.darkGrey,
+                    fontSize: 16.0,
+                  ),
+                ),
+                const Icon(Icons.attach_file, color: AppColors.darkGrey),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class PinPointInput extends StatelessWidget {
+  final String label;
+  final String hintText;
+
+  const PinPointInput({
+    super.key,
+    required this.label,
+    required this.hintText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TextSmallMedium(text: label),
+        TextFormField(
+          decoration: InputDecoration(
+            hintText: hintText,
+            hintStyle: const TextStyle(
+              color: Color(0xFF7C7C7C),
+              fontSize: 16.0,
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8.0),
+              borderSide: const BorderSide(
+                color: Color(0xFFD9D9D9),
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8.0),
+              borderSide: const BorderSide(
+                color: Color(0xFF0984E3),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/ui/penemuan_form_page.dart
+++ lib/ui/penemuan_form_page.dart
@@ -1,16 +1,118 @@
 import 'package:flutter/material.dart';
+import 'package:temulik/ui/components/components.dart';
 
-class PenemuanFormPage extends StatelessWidget {
+class PenemuanFormPage extends StatefulWidget {
   const PenemuanFormPage({super.key});
+
+  @override
+  State<PenemuanFormPage> createState() => _PenemuanFormPageState();
+}
+
+class _PenemuanFormPageState extends State<PenemuanFormPage> {
+  String? selectedValue;
+  DateTime? _selectedDate;
+  TimeOfDay? _selectedTime;
+  String? _selectedImagePath;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Form Penemuan'),
+      appBar: LaporAppBar(
+        title: 'Lapor Barang Temuan',
       ),
-      body: Center(
-        child: Text('Penemuan Form Page'),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 20.0,
+            right: 20.0,
+            bottom: 20.0,
+          ),
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Column(
+              children: [
+                InputForm(
+                  label: 'Nama Barang',
+                  hintText: 'ex: Iphone 12 Pro Max',
+                ),
+                SizedBox(height: 16.0),
+                SelectForm(
+                  label: 'Kategori',
+                  hintText: 'Pilih Kategori',
+                  items: [
+                    'Elektronik',
+                    'Buku',
+                    'Pakaian',
+                    'Lainnya',
+                  ],
+                  value: selectedValue,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedValue = newValue;
+                    });
+                  },
+                ),
+                SizedBox(height: 16.0),
+                TextAreaForm(
+                  label: 'Deskripsi',
+                  hintText: 'ex: ukuran, ciri khas, atau tanda pengenal',
+                  maxLength: 100,
+                  onChanged: (value) {
+                    print(value);
+                  },
+                ),
+                SizedBox(height: 16.0),
+                ImagePickerForm(
+                  label: 'Foto Barang',
+                  hintText: 'Unggah Foto',
+                  imagePath: _selectedImagePath,
+                  onImageSelected: (String? path) {
+                    setState(() {
+                      _selectedImagePath = path;
+                    });
+                  },
+                ),
+                SizedBox(height: 16.0),
+                DatePickerForm(
+                  label: 'Tanggal Kehilangan Barang',
+                  hintText: 'Pilih Tanggal',
+                  selectedDate: _selectedDate, // DateTime? variable
+                  onChanged: (date) {
+                    setState(() {
+                      _selectedDate = date;
+                    });
+                  },
+                ),
+                SizedBox(height: 16.0),
+                TimePickerForm(
+                  label: 'Jam Kehilangan Barang',
+                  hintText: 'Pilih Jam',
+                  selectedTime: _selectedTime,
+                  onChanged: (time) {
+                    setState(() {
+                      _selectedTime = time;
+                    });
+                  },
+                ),
+                SizedBox(height: 16.0),
+                PinPointInput(
+                  label: 'Pin Point',
+                  hintText: 'Tentukan Pin Point Lokasi',
+                ),
+                SizedBox(height: 16.0),
+                InputForm(
+                  label: 'no. WhatsApp',
+                  hintText: 'format: 08xxxxxxxxxx',
+                ),
+                SizedBox(height: 16.0),
+                InputForm(
+                  label: 'Imbalan',
+                  hintText: 'ex: Rp. 500.000 (opsional)',
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
